8b0cfa9ef886c3443cf0b8d3757d1d5a972b6df2
ChristianMendez1 <ChristianMendez1213@gmail.com>
2023-04-12 10:48:21 -0400

    tookout some comments

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -7,9 +7,7 @@ function Sidebar() {
         { name: "Online Analysis"},
         { name: "Settings"},
       ]
-      // map over the dogs array and create an array of JSX for each dog
       const navJSX = navs.map(nav => {
-        // we return JSX for each dog in the array which we store in the dog variable, essentially we are looping over dog of dogs
         return (
           <div>
             <ul>
@@ -18,8 +16,6 @@ function Sidebar() {
           </div>
         )
       })
-    
-      // the component returns JSX that uses the dogJSX array
       return <div id='sidebar'>{navJSX}</div>
     }
 
